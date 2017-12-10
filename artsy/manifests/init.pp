8206467d630244c8caa13957e384487fdb8da025
inaandersin <32093361+inaandersin@users.noreply.github.com>
2017-12-10 22:24:35 +0000

    Add files via upload

---
+++ artsy/manifests/init.pp
@@ -0,0 +1,26 @@
+class artsy {
+	exec {'apt-get update':
+	command => '/usr/bin/apt-get update',
+	refreshonly => true,
+	}        
+
+	package {'krita':
+        ensure => 'installed',
+        allowcdrom => 'true',
+        }
+
+	file { "/usr/share/xfce4/backdrops/wallpaper.png":
+                source => "/etc/puppet/modules/artsy/files/Untitled_by_Aaron_Burde$
+        }
+
+        file { "/home/xubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-de$
+                content => template("bg/xfce4-desktop.xml"),
+        }
+
+
+	file {'/media/xubuntu/F5AB-1DB5/':
+        ensure => 'directory',
+        source => '/home/xubuntu/Pictures/',
+        recurse => 'remote',
+        }
+}
