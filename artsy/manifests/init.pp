99696551e6007168533b57adc04cd0a1e07af308
inaandersin <32093361+inaandersin@users.noreply.github.com>
2017-12-10 22:57:48 +0000

    Update init.pp

--- artsy/manifests/init.pp
+++ artsy/manifests/init.pp
@@ -8,6 +8,11 @@ class artsy {
         ensure => 'installed',
         allowcdrom => 'true',
         }
+	
+	package {'gimp':
+        ensure => 'installed',
+        allowcdrom => 'true',
+        }
 
 	file { "/usr/share/xfce4/backdrops/wallpaper.png":
                 source => "/etc/puppet/modules/artsy/files/Untitled_by_Aaron_Burden.jpg",
@@ -16,7 +21,6 @@ class artsy {
         file { "/home/xubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml":
                 content => template("artsy/xfce4-desktop.xml"),
         }
-
 
 	file {'/media/xubuntu/F5AB-1DB5/':
         ensure => 'directory',
